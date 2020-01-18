436492a5d146176bf62300e4c4416d959c351459
maoshize0543@sina.com <shizemao@creditease.cn>
2020-01-18 11:07:49 +0800

    code

---
+++ src/main/java/algorithm/array/Leetcode49.java
@@ -0,0 +1,52 @@
+package algorithm.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @program: algorithm
+ * @description: 字母异位词分组
+ * @author: szm
+ * @create: 2020-01-18 10:36
+ **/
+public class Leetcode49 {
+
+    @Test
+    public void test() {
+        System.out.println("----------start-----------");
+        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = groupAnagrams(input);
+        System.out.println(lists);
+    }
+
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // 通过 hash表 + 字符串排序
+        List<List<String>> res = new LinkedList<>();
+        if (strs.length < 1) return res;
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            String helper = helper(strs[i]);
+            if (!map.containsKey(helper)) {
+                List<String> list = new LinkedList<>();
+                list.add(strs[i]);
+                map.put(helper, list);
+            } else {
+                map.get(helper).add(strs[i]);
+            }
+        }
+        res.addAll(map.values());
+        return res;
+    }
+
+    private String helper(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return String.valueOf(chars);
+    }
+
+}
---
+++ src/main/java/algorithm/dynamicprogram/Leetcode122.java
@@ -0,0 +1,22 @@
+package algorithm.dynamicprogram;
+
+/**
+ * @program: algorithm
+ * @description: 买卖股票的最佳时机 II 多次买卖一支股票,再次购买前出售掉之前的股票
+ * @author: szm
+ * @create: 2020-01-18 10:23
+ **/
+public class Leetcode122 {
+
+    public int maxProfit(int[] prices) {
+        // 通过
+        if(prices.length < 2) return 0;
+        // 最大利润
+        int maxProfit = 0;
+        for(int i = 1; i < prices.length; i ++) {
+            // 如果买入赚钱就叠加到maxProfit中
+            maxProfit = Math.max(maxProfit, maxProfit + (prices[i] - prices[i - 1]));
+        }
+        return maxProfit;
+    }
+}
